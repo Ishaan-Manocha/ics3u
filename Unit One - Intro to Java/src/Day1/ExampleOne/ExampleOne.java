13cb12d7f3226b76a86a1ee2c19799ee81e5cef3
Ishaan Manocha <imanocha@bayviewglen.ca>
2024-09-09 10:32:35 -0400

    Initial Commit

---
+++ Unit One - Intro to Java/src/Day1/ExampleOne/ExampleOne.java
@@ -0,0 +1,7 @@
+package Day1.ExampleOne;
+
+public class ExampleOne {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Unit One - Intro to Java/src/ExampleTwo/ExampleTwo.java
@@ -0,0 +1,11 @@
+package ExampleTwo;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("This is great");
+        System.out.println("This is okay");
+        System.out.print("This is okay");
+        System.out.print("This is not okay");
+        
+    }
+}
